a38933b4cfa5c45bae74ba15d9a934475a85111a
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-03-10 16:14:27 -0400

    changed to backbone.router, maintained functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-var App = (function(){
+var App = Backbone.Router.extend({
 
-  function App() {
-    _.extend(this, Backbone.Events);
+    routes: {
+    ""          : "showHome",
+    "products"  : "showProducts",
+    "terms"     : "showTerms"
+  },
 
-    // views
 
+  initialize: function(){
     this.products     = new Products();
     this.productsView = new ProductsView({collection: this.products});
     this.nav          = new NavView();
@@ -44,27 +47,97 @@ var App = (function(){
     // default to showing home
 
     this.showHome();
+  },
+
+  showProducts: function() {
+    this.$main.html( this.productsView.render().el );
+    if( !this.products.length ) {
+      this.products.fetch();
+    }
+  },
+
+  showHome: function() {
+    this.$main.html( this.homeView.render().el );
+  },
+
+  showTerms: function() {
+    this.$main.html( this.termsView.render().el );
   }
 
-  App.prototype = {
+});
 
-    showProducts: function() {
-      this.$main.html( this.productsView.render().el );
-      if( !this.products.length ) {
-        this.products.fetch();
-      }
-    },
 
-    showHome: function() {
-      this.$main.html( this.homeView.render().el );
-    },
 
-    showTerms: function() {
-      this.$main.html( this.termsView.render().el );
-    }
 
-  };
 
-  return App;
 
-})();
+// var App = (function(){
+
+//   function App() {
+//     _.extend(this, Backbone.Events);
+
+//     // views
+
+//     this.products     = new Products();
+//     this.productsView = new ProductsView({collection: this.products});
+//     this.nav          = new NavView();
+//     this.homeView     = new HomeView();
+//     this.termsView    = new TermsView();
+
+//     // initial structure
+
+//     $("body").html( JST.app() );
+//     $("header").html( this.nav.render().el );
+//     this.$main = $(".main");
+
+//     // listeners
+
+//     this.listenTo(this.products, "request", function(){
+//       this.nav.showSpinner();
+//     });
+
+//     this.listenTo(this.products, "sync", function(){
+//       this.nav.hideSpinner();
+//     });
+
+//     this.listenTo(this.nav, "link:click", function(name){
+//       switch(name) {
+//         case "products":
+//           this.showProducts();
+//         break;
+//         case "terms":
+//           this.showTerms();
+//         break;
+//         default:
+//           this.showHome();
+//         break;
+//       }
+//     });
+
+    // default to showing home
+
+  //   this.showHome();
+  // }
+
+  // App.prototype = {
+
+  //   showProducts: function() {
+  //     this.$main.html( this.productsView.render().el );
+  //     if( !this.products.length ) {
+  //       this.products.fetch();
+  //     }
+  //   },
+
+  //   showHome: function() {
+  //     this.$main.html( this.homeView.render().el );
+  //   },
+
+  //   showTerms: function() {
+  //     this.$main.html( this.termsView.render().el );
+  //   }
+
+  // };
+
+//   return App;
+
+// })();
